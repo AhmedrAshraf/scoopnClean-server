18f5549872629106265ba52f3e09b397653a1178
Ahmed Raza <ahmedr.0331@gmail.com>
2025-03-23 02:20:50 +0500

    success_url fixed

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -33,7 +33,7 @@ app.post('/api/create-checkout-session', async(req, res)=>{
         // success_url: 'http://localhost:8100/api/success-payment?session_id={CHECKOUT_SESSION_ID}',
         // cancel_url: 'http://localhost:5173/cancel',
 
-        success_url: 'https://scoopn-clean.vercel.app/api/success-payment?session_id={CHECKOUT_SESSION_ID}',
+        success_url: 'https://scoopn-clean-server.vercel.app/api/success-payment?session_id={CHECKOUT_SESSION_ID}',
         cancel_url: 'https://scoopn-clean.vercel.app/cancel',
 
         customer_email: `${customerEmail}`,
